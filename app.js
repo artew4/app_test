acec3a1009c259e3ebcc396c3fd1db2609cef110
Artem <118404061+artew4@users.noreply.github.com>
2024-06-09 19:43:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+alert('wooooo');
+
 let tg = window.Telegram.WebApp;
 let text_id = document.getElementById('text_id');
 let text_name = document.getElementById('text_name');
+var testing = document.getElementById("testing");
 
-var user_id = tg.initDataUnsafe.user.id;
-var user_name = tg.initDataUnsafe.user.first_name;
+//console.log(testing)
+
+var user_id = tg.initData.user.id;
+var user_name = tg.initData.user.first_name;
+var some_text = 'some text from js';
 
 text_id.innerHTML = user_id;
 text_name.innerHTML = user_name;
+testing.innerHTML = some_text;
